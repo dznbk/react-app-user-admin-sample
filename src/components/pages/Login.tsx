06275e7cbde60b8a72889857986e2ae61a7d0bd6
D-PONTARO <46421953+dznbk@users.noreply.github.com>
2022-02-03 06:07:28 +0900

    ログイン機能の実装

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,16 +1,18 @@
-import {
-  Box,
-  Button,
-  Divider,
-  Flex,
-  Heading,
-  Input,
-  Stack
-} from "@chakra-ui/react";
-import { memo, VFC } from "react";
+import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
+import { ChangeEvent, memo, VFC, useState } from "react";
 import { PrimaryButton } from "./../atoms/button/PrimaryButton";
+import { useAuth } from "./../../hooks/useAuth";
 
 export const Login: VFC = memo(() => {
+  const [userId, setUserId] = useState("");
+  const { login, loading } = useAuth();
+
+  const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserId(e.target.value);
+  };
+
+  const onClickLogin = () => login(userId);
+
   return (
     <Flex align="center" justify="center" height="100vh">
       <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
@@ -19,8 +21,14 @@ export const Login: VFC = memo(() => {
         </Heading>
         <Divider my={4} />
         <Stack spacing={6} py={4} px={10}>
-          <Input placeContent="ユーザーID" />
-          <PrimaryButton>ログイン</PrimaryButton>
+          <Input placeContent="ユーザーID" onChange={onChangeUserId} />
+          <PrimaryButton
+            disabled={userId === ""}
+            loading={loading}
+            onClick={onClickLogin}
+          >
+            ログイン
+          </PrimaryButton>
         </Stack>
       </Box>
     </Flex>
