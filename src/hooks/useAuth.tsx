1e010d6721cb78f5751de297f6a6b924271912a7
D-PONTARO <46421953+dznbk@users.noreply.github.com>
2022-02-03 06:15:32 +0900

    ログイン時のメッセージ表示

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -2,29 +2,36 @@ import axios from "axios";
 import { useCallback, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { User } from "./../types/user";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const history = useHistory();
 
   const [loading, setLoading] = useState(false);
 
-  const login = useCallback((id: string) => {
-    setLoading(true);
-    axios
-      .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then((res) => {
-        if (res.data) {
-          history.push("/home");
-        } else {
-          alert("ユーザーが見つかりません");
-        }
-      })
-      .catch(() => {
-        alert("ログインできません");
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
+  const { showMessage } = useMessage();
+
+  const login = useCallback(
+    (id: string) => {
+      setLoading(true);
+      axios
+        .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then((res) => {
+          if (res.data) {
+            showMessage({ title: "ログインしました", status: "success" });
+            history.push("/home");
+          } else {
+            showMessage({ title: "ユーザーが見つかりません", status: "error" });
+          }
+        })
+        .catch(() => {
+          showMessage({ title: "ログインできません", status: "error" });
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    },
+    [history, showMessage]
+  );
   return { login, loading };
 };
