eac16cb6bc9b1adedb741cecf13cbc2b1f957d57
D-PONTARO <46421953+dznbk@users.noreply.github.com>
2022-02-03 08:28:14 +0900

    ログインユーザーをContextに保持する

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -3,12 +3,13 @@ import { useCallback, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { User } from "./../types/user";
 import { useMessage } from "./useMessage";
+import { useLoginUser } from "./../hooks/useLoginUser";
 
 export const useAuth = () => {
   const history = useHistory();
 
   const [loading, setLoading] = useState(false);
-
+  const { setLoginUser } = useLoginUser();
   const { showMessage } = useMessage();
 
   const login = useCallback(
@@ -18,20 +19,20 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            setLoginUser(res.data);
             showMessage({ title: "ログインしました", status: "success" });
             history.push("/home");
           } else {
             showMessage({ title: "ユーザーが見つかりません", status: "error" });
+            setLoading(false);
           }
         })
         .catch(() => {
-          showMessage({ title: "ログインできません", status: "error" });
-        })
-        .finally(() => {
           setLoading(false);
+          showMessage({ title: "ログインできません", status: "error" });
         });
     },
-    [history, showMessage]
+    [history, showMessage, setLoginUser]
   );
   return { login, loading };
 };
